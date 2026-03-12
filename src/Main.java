57cfe40bc09c0fce7a3a76072a8f763431a180ef
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-03-12 20:49:16 -0300

    Soma de dois numeros

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public  class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World !");
+        int num1 = 5;
+        int num2 = 10;
+
+        System.out.println(num1+num2);
     }
 }
