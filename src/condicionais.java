2aea74fa566d629101f5f8519898507713854da4
Guilherme Secone <guilhermeseconeneves@gmail.com>
2026-03-12 21:00:24 -0300

    condicionais

---
+++ src/condicionais.java
@@ -0,0 +1,21 @@
+public class condicionais {
+     static void main(String[] args) {
+
+        int anoNascimento = 2009;
+
+        if (anoNascimento < 2008){
+
+            System.out.println("Você ja deve ter se alistado");
+        }
+
+        else if (anoNascimento == 2008){
+
+            System.out.println("Se alistar esse ano");
+        }
+
+        else if (anoNascimento> 2008) {
+             System.out.println("Voce ainda nao vai se alistar");
+         }
+
+    }
+}
